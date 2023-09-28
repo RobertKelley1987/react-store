be2f5a76a651288ca7d8218023902e3c2a872ba8
Robert Kelley <robertkelley1987@gmail.com>
2023-09-28 16:27:15 -0400

    changed api url

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const productionURL = 'https://react-store-lyje.onrender.com/';
+const productionURL = 'https://infinite-bliss-react-store.onrender.com/';
 
 export const api = axios.create({
     baseURL: productionURL, 
